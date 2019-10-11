b8af0e9a9189445a787252711a367a4ea7b2735d
Gisli Thor <gislig18@ru.is>
2019-10-11 09:48:46 +0000

    First comit

---
+++ Airline_seating.py
@@ -0,0 +1,28 @@
+def input_plane():
+    rows = input("Enter number of rows: ")
+    seats = input("Enter number of seats in each row: ")
+    return rows , seats
+
+def choose_seats(rows,seats):
+    loop = True
+    while loop == True:
+        seat = input("Input seat number (row seat): ")
+        more = input("More seats (y/n)? ")
+        if more == n
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    rows, seats = input_plane()
