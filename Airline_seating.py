67184ed274cf3a17c9cfe6ca4d95ae4fd88da08b
Gisli Thor <gislig18@ru.is>
2019-10-14 13:27:04 +0000

    Búið

--- Airline_seating.py
+++ Airline_seating.py
@@ -15,18 +15,19 @@ def to_seats_list(big_list,rows,seats): # Notum raða- og sætafjöldan til að 
         counter +=1
     return seat_list
 
-def print_seats(seat_list):
+
+def print_seats(seat_list): # Here we split the newest seed list into 2 aisles and print them out
     meat_list = list(seat_list)
     counter = 0
     half_len = len(meat_list[0]) // 2
     for i in range(len(meat_list)):
-        seat_row_1 =''.join(meat_list[i][0:half_len])
-        seat_row_2 =''.join(meat_list[i][half_len:])
+        seat_row_1 =' '.join(meat_list[i][0:half_len])
+        seat_row_2 =' '.join(meat_list[i][half_len:])
         counter += 1
-        print("{0:2},{1:4},{2:4}".format(counter,seat_row_1,seat_row_2))
+        print(("{0:2}" +"   "+ "{1:1}" +"   "+ "{2:1}"+" ").format(counter,seat_row_1,seat_row_2))
 
 
-def reserve_seat(seat_list, chosen):
+def reserve_seat(seat_list, chosen): # Here we use the enumerate function to cahnge the seat list when a seat is reserved
     seat_list = list(seat_list)
     chosen = list(chosen)
     for x in range(len(seat_list)):
@@ -35,27 +36,63 @@ def reserve_seat(seat_list, chosen):
                 if i == chosen[1]:
                     seat_list[x][n] = "X"
 
-def choose_seats(seat_list):
+
+def check_seat(big_list,seat_list):
+    big_list = list(big_list)
+    seat_list = list(seat_list)
+    index = 0
     loop = True
+    print_seats(seat_list)
     while loop == True:
-        #búa til fall til að prenta út listann
-        print_seats(seat_list)
-        seat = input("Input seat number (row seat): ")
-        chosen = seat.split(" ")
-        if int(chosen[0]) < 1 or int(chosen[0]) > len(seat_list) or chosen[1] not in seat_list[int(chosen[0])-1]:
-            print("Invalid seat!")
-        #búa til fall til að taka frá sæti
-        reserve_seat(seat_list,chosen)
-        more = input("More seats (y/n)? ")
-        if more == "n":
-            loop = False
+        full_plane = 0
+        for i in seat_list:
+            for x in i:
+                if x != "X":
+                    full_plane +=1
+        if full_plane != 0:
+            seat = input("Input seat number (row seat): ")
+            chosen = seat.split(" ")
+            
+            try:
+                for i in range(len(big_list)):
+                    if big_list[i] == chosen[1]:
+                        index = i
+                if seat_list[int(chosen[0])-1][index] == "X":
+                    print("That seat is taken!")
+                else:
+                    if int(chosen[0]) < 1 or int(chosen[0]) > len(seat_list) or chosen[1] not in seat_list[int(chosen[0])-1]:
+                        print("Seat number is invalid!")
+                    else:
+                        reserve_seat(seat_list,chosen)
+                        print_seats(seat_list)
+                        full_plane = 0
+                        for i in seat_list:
+                            for x in i:
+                                if x != "X":
+                                    full_plane +=1
+                        if full_plane != 0:
+                            more = input("More seats (y/n)? ")
+                            if more != "y":
+                                loop = False
+                        else:
+                            loop = False
+                           
+                                      
+            except ValueError:
+                    print("Seat number is invalid!")
+            except IndexError:
+                    print("Seat number is invalid!")
+        else:
+            print_seats(seat_list)
         
-
-
+    
+    
+    
+        
 
 def main():
     rows, seats = input_plane()
     seat_list = to_seats_list(big_list,rows,seats)
-    choose_seats(seat_list)
+    check_seat(big_list,seat_list)
 
 main()
