1585eaa60b471d2ae512f7a49cc9dd467344fe92
Gisli Thor <gislig18@ru.is>
2019-10-14 09:57:37 +0000

    reyna join

--- Airline_seating.py
+++ Airline_seating.py
@@ -18,8 +18,7 @@ def to_seats_list(big_list,rows,seats): # Notum raða- og sætafjöldan til að 
 def print_seats(seat_list):
     meat_list = list(seat_list)
     counter = 0
-    seat_row_1 =" ".join(meat_list)
-    seat_row_2 =" ".join(meat_list)
+    seat_row_1 =''.join(meat_list)
     for i in meat_list:
         counter += 1
         print("{0:2},{1:4},{2:4}".format(counter,"ABC","DEF"))
